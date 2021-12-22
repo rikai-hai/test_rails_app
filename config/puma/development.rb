e70e86cc2b43db7fd7238b98de87200394d10ee4
Hai.ky <hai.ky@rikai.technology>
2021-12-22 15:06:25 +0700

    ESG-3948 SS|商品の税区分を連携するようにする

--- config/puma/development.rb
+++ config/puma/development.rb
@@ -7,16 +7,14 @@ threads threads_count, threads_count
 preload_app!
 bind  "unix:///var/tmp/test_rails_app.sock"
 pidfile "/var/run/puma/test_rails_app.pid"
-stdout_redirect "/deploy/apps/test_rails_app/repo/test_rails_app/log/test_rails_app.log"
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Valid on Rails up to 4.1 the initializer method of setting `pool` size
   ActiveSupport.on_load(:active_record) do
     config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
+      Rails.application.config.database_configuration[Rails.env]
     ActiveRecord::Base.establish_connection(config)
   end
 end
